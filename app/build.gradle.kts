23522f11a307e31d674f9c1968d123270f67fc7d
Nullptr <noreply@nullptr.icu>
2026-07-04 13:44:55 +0200

    Add release signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,19 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.agp.app)
 }
+
+val localProperties = Properties().apply {
+    rootProject.file("local.properties")
+        .inputStream()
+        .use { load(it) }
+}
+
+fun localProperty(name: String): String? =
+    localProperties.getProperty(name)?.takeIf { it.isNotBlank() }
+
+val hasSigningConfig = localProperty("androidStoreFile") != null
 
 android {
     namespace = "icu.nullptr.polyglot"
@@ -18,7 +31,23 @@ android {
         buildConfig = true
     }
 
+    signingConfigs {
+        if (hasSigningConfig) {
+            create("release") {
+                storeFile = file(localProperty("androidStoreFile")!!)
+                storePassword = localProperty("androidStorePassword")
+                keyAlias = localProperty("androidKeyAlias")
+                keyPassword = localProperty("androidKeyPassword")
+            }
+        }
+    }
+
     buildTypes {
+        if (hasSigningConfig) {
+            all {
+                signingConfig = signingConfigs.getByName("release")
+            }
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
